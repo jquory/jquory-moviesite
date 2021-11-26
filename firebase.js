d9943347641caeae831e67afc8be0ef4f791b04d
Qorey <77075181+Qorey@users.noreply.github.com>
2021-11-26 07:32:55 +0700

    update config firebase

--- firebase.js
+++ firebase.js
@@ -1,12 +1,18 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBYdm-3YCZoNQn2WCSoRkXC_hpf7ewrItk",
-    authDomain: "project-film-b58ff.firebaseapp.com",
-    projectId: "project-film-b58ff",
-    storageBucket: "project-film-b58ff.appspot.com",
-    messagingSenderId: "219777321220",
-    appId: "1:219777321220:web:82f6b51ee0c79ffcef2851"
-  };
+  apiKey: "AIzaSyBYdm-3YCZoNQn2WCSoRkXC_hpf7ewrItk",
+  authDomain: "project-film-b58ff.firebaseapp.com",
+  projectId: "project-film-b58ff",
+  storageBucket: "project-film-b58ff.appspot.com",
+  messagingSenderId: "219777321220",
+  appId: "1:219777321220:web:82f6b51ee0c79ffcef2851"
+};
 
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
 
+const db = app.firestore();
 
-export default firebaseConfig 
+export { db };
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,23 +1,17 @@
-import NextAuth from "next-auth"
-import GoogleProviders from "next-auth/providers/google"
-import { FirebaseAdapter } from '@next-auth/firebase-adapter'
-import firebaseConfig from "../../../firebase"
-
-import firebase from "firebase/app"
-import "firebase/firestore"
-
-const firestore = (
-  firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
-).firestore()
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { db } from "../../../firebase";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProviders({
+    Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    // ...add more providers here
   ],
-  adapter: FirebaseAdapter(firestore)
-})
 
+  adapter: FirebaseAdapter(db),
+});
