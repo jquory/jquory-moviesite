7003b083b974d1b950d876f5e62dc0a75a7a7d5f
Qorey <77075181+Qorey@users.noreply.github.com>
2021-11-26 21:11:26 +0700

    update auth dan firebase

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,47 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import { db } from "../../../firebase";
-import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { FirebaseAdapter } from "@next-auth/firebase-adapter"
 
+import firebase from "firebase/app"
+import "firebase/firestore"
+import firebaseConfig from '../../../firebase'
+
+const firestore = (
+  firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
+).firestore()
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
 export default NextAuth({
-  // Configure one or more authentication providers
+  // https://next-auth.js.org/providers/overview
   providers: [
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
   ],
+  adapter: FirebaseAdapter(firestore),
+})
 
-  adapter: FirebaseAdapter(db),
-});
+
+
+
+
+
+// import NextAuth from "next-auth";
+// import Providers from "next-auth/providers";
+// import { db } from "../../../firebase";
+// import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+
+// export default NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     Providers.Google({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+
+//   adapter: FirebaseAdapter(db),
+// });
